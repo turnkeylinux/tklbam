01646938cbd5f5a7f284f53884a27b7d9711ba95
Liraz Siri <liraz@turnkeylinux.org>
2010-02-14 19:47:20 +0200

    save absolute path and use lstat instead of stat (don't follow links)

--- dirindex.py
+++ dirindex.py
@@ -6,6 +6,6 @@ def create(path_index, paths):
     for path in paths:
         for dpath, dnames, fnames in os.walk(path):
             for fname in fnames:
-                path = join(dpath, fname)
-                st = os.stat(path)
+                path = abspath(join(dpath, fname))
+                st = os.lstat(path)
                 print >> fh, "%s\t%d\t%d" % (path, st.st_mtime, st.st_size)
