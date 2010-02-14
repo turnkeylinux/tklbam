deabfca51388086572452280d0ab797b8d84fc1a
Liraz Siri <liraz@turnkeylinux.org>
2010-02-14 20:26:43 +0200

    1/2 refactor: redesigned DirIndex interface (not implemented yet)

--- dirindex.py
+++ dirindex.py
@@ -1,6 +1,19 @@
 import os
 from os.path import *
 
+class Error(Exception):
+    pass
+
+class DirIndex(dict):
+    def __init__(self, fromfile=None):
+        pass
+
+    def walk(self, *paths):
+        pass
+
+    def save(self, tofile):
+        pass
+
 def create(path_index, paths):
     fh = file(path_index, "w" )
     for path in paths:
@@ -10,8 +23,14 @@ def create(path_index, paths):
                 st = os.lstat(path)
                 print >> fh, "%s\t%d\t%d" % (path, st.st_mtime, st.st_size)
 
+def _create(path_index, paths):
+    di = DirIndex()
+    di.walk(*paths)
+    di.save(path_index)
+
 def compare(path_index, paths):
     map_index = {}
+
     for line in file(path_index).readlines():
         path, mtime, size = line.split('\t')
         map_index[path] = (int(mtime), int(size))
@@ -30,6 +49,19 @@ def compare(path_index, paths):
     samepaths = set(map_fs) & set(map_index)
     for path in samepaths:
         if map_index[path] != map_fs[path]:
+            delta.append(path)
+    
+    return delta
+
+def _compare(path_index, paths):
+    di_saved = DirIndex(path_index)
+    di_fs = DirIndex()
+    di_fs.walk(*paths)
+
+    delta = list(set(di_fs) - set(di_saved))
+    samepaths = set(di_fs) & set(di_index)
+    for path in samepaths:
+        if di_index[path] != di_fs[path]:
             delta.append(path)
     
     return delta
