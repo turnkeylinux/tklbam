85fa41124c9bdabe93bc9e3376e8530a3de27791
Liraz Siri <liraz@turnkeylinux.org>
2010-02-15 21:07:35 +0200

    added support for inputting paths from a file

--- whatchanged.py
+++ whatchanged.py
@@ -2,9 +2,12 @@
 """Print a list of files that have changed
 
 Options:
-    --create    create index
+    -i --input=PATH     Read a list of paths frmo a file (- for stdin)
+
+    --create            Create index
 
 """
+import re
 import sys
 import getopt
 import dirindex 
@@ -21,27 +24,51 @@ def fatal(s):
     print >> sys.stderr, "error: " + str(s)
     sys.exit(1)
 
+def parse_input(inputfile):
+    paths = []
+    
+    if inputfile == '-':
+        fh = sys.stdin
+    else:
+        fh = file(inputfile)
+
+    for line in fh.readlines():
+        line = re.sub(r'#.*', '', line).strip()
+        if not line:
+            continue
+
+        paths.append(line)
+
+    return paths
+
 def main():
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'c:h', 
-                                       ['create'])
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'i:c:h', 
+                                       ['create', 'input='])
     except getopt.GetoptError, e:
         usage(e)
 
     opt_create = False
+    opt_input = None
 
     for opt, val in opts:
-        if opt == '-h':
+        if opt in ('-h', '--help'):
             usage()
 
         elif opt in ('-c', '--create'):
             opt_create = True
 
-    if len(args) < 2:
+        elif opt in ('-i', '--input'):
+            opt_input = val
+
+    if not args or (not opt_input and len(args) < 2):
         usage()
 
     path_index = args[0]
     paths = args[1:]
+    
+    if opt_input:
+        paths += parse_input(opt_input)
 
     if opt_create:
         dirindex.create(path_index, paths)
