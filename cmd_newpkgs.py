93f1f052d5db2b1443d3e391fc581c8a10b344ad
Liraz Siri <liraz@turnkeylinux.org>
2010-03-05 14:31:26 +0200

    implemented newpkgs logic

--- cmd_newpkgs.py
+++ cmd_newpkgs.py
@@ -1,40 +1,67 @@
 #!/usr/bin/python
 """Print list of new packages"""
+import os
 import sys
 import commands
+
+import re
 
 def usage(e=None):
     if e:
         print >> sys.stderr, e
 
-    print >> sys.stderr, "Syntax: %s base-selections [ old-selections ]" % sys.argv[0]
+    print >> sys.stderr, "Syntax: %s base-selections [ selections ]" % sys.argv[0]
     print >> sys.stderr, __doc__.strip()
     sys.exit(1)
 
 class Error(Exception):
     pass
 
-def get_selections():
-    cmd = "dpkg --get-selections"
-    errno, output = commands.getstatusoutput(cmd)
-    if errno:
-        raise Error("command failed (%d): %s" % (errno, cmd))
+class DpkgSelections(set):
+    @staticmethod
+    def _parse(buf):
+        for line in buf.strip().split('\n'):
+            package, state = re.split(r'\t+', line)
+            if state == 'deinstall':
+                continue
+            yield package
 
-    return output
+    @staticmethod
+    def _dpkg_get_selections():
+        cmd = "dpkg --get-selections"
+        errno, output = commands.getstatusoutput(cmd)
+        if errno:
+            raise Error("command failed (%d): %s" % (errno, cmd))
+
+        return output
+
+    def __init__(self, arg=None):
+        """If arg is not provided we get selections from dpkg.
+           arg can be a filename, a string."""
+
+        if arg:
+            if os.path.exists(arg):
+                buf = file(arg).read()
+            else:
+                buf = arg
+        else:
+            buf = self._dpkg_get_selections()
+
+        set.__init__(self, self._parse(buf))
 
 def main():
     args = sys.argv[1:]
     if len(args) not in (1, 2):
         usage()
 
-    base_selections = args[0]
+    base_selections = DpkgSelections(args[0])
     try:
-        old_selections = args[1]
+        selections = DpkgSelections(args[1])
     except:
-        old_selections = None
+        selections = DpkgSelections()
 
-    print `base_selections, old_selections`
-
+    for package in (selections - base_selections):
+        print package
         
 if __name__=="__main__":
     main()
