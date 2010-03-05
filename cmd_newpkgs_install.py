b83938cb6f5b12eac997e013a0db13b0420e8aae
Liraz Siri <liraz@turnkeylinux.org>
2010-03-05 15:47:15 +0200

    implemented cli stub for newpkgs-install command

---
+++ cmd_newpkgs_install.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+"""
+Install list of new packages that are available in apt-cache.
+
+Options:
+    -i --input=PATH         Read a list of packages from a file (- for stdin)
+    -v --verbose            Turn on verbosity
+    -s --simulate           Don't execute apt-get
+"""
+
+import re
+import sys
+import getopt
+
+def usage(e=None):
+    if e:
+        print >> sys.stderr, e
+
+    print >> sys.stderr, "Syntax: %s [ -options ] [ package-name ... ]" % sys.argv[0]
+    print >> sys.stderr, __doc__.strip()
+    sys.exit(1)
+
+def parse_input(inputfile):
+    packages = []
+    
+    if inputfile == '-':
+        fh = sys.stdin
+    else:
+        fh = file(inputfile)
+
+    for line in fh.readlines():
+        line = re.sub(r'#.*', '', line).strip()
+        if not line:
+            continue
+
+        packages.append(line)
+
+    return packages
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'i:svh', 
+                                       ['input=', 'simulate', 'verbose'])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    opt_input = None
+    opt_simulate = False
+    opt_verbose = False
+
+    for opt, val in opts:
+        if opt in ('-i', '--input'):
+            opt_input=val
+
+        elif opt in ('-s', '--simulate'):
+            opt_simulate = True
+
+        elif opt in ('-v', '--verbose'):
+            opt_verbose = True
+        else:
+            usage()
+
+    if opt_simulate:
+        opt_verbose = True
+
+    if not args and not opt_input:
+        usage()
+
+    packages = args
+    if opt_input:
+        packages += parse_input(opt_input)
+
+    print `(opt_verbose, opt_simulate)`
+    print `packages`
+
+if __name__=="__main__":
+    main()
+
