6e349785907f3612c9082546bdd110493821ee73
Liraz Siri <liraz@turnkeylinux.org>
2010-03-14 02:38:54 +0200

    restore: cli stub

---
+++ cmd_restore.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+"""
+Restore backup
+
+Options:
+    --skip-files                Don't restore filesystem
+    --skip-database             Don't restore databases
+    --skip-packages             Don't restore new packages
+
+    --no-rollback               Disable rollback
+
+"""
+
+from os.path import *
+
+import sys
+import getopt
+
+def fatal(e):
+    print >> sys.stderr, "error: " + str(e)
+    sys.exit(1)
+
+def usage(e=None):
+    if e:
+        print >> sys.stderr, "error: " + str(e)
+
+    print >> sys.stderr, "Syntax: %s [ -options ] <address> <keyfile> [ limits ... ]" % sys.argv[0]
+    print >> sys.stderr, __doc__.strip()
+    sys.exit(1)
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'h', 
+                                       ['skip-files', 'skip-database', 'skip-packages',
+                                        'no-rollback'])
+                                        
+    except getopt.GetoptError, e:
+        usage(e)
+
+    skip_files = False
+    skip_database = False
+    skip_packages = False
+    no_rollback = False
+    for opt, val in opts:
+        if opt == '--skip-files':
+            skip_files = True
+        elif opt == '--skip-database':
+            skip_database = True
+        elif opt == '--skip-packages':
+            skip_packages = True
+        elif opt == '--no-rollback':
+            no_rollback = True
+        elif opt == '-h':
+            usage()
+
+    if len(args) < 2:
+        usage()
+
+    address, keyfile = args[:2]
+    limits = args[2:]
+
+    # debug
+    for var in ('address', 'keyfile', 'limits', 'skip_files', 'skip_database', 'skip_packages', 'no_rollback'):
+        print "%s = %s" % (var, `locals()[var]`)
+
+if __name__=="__main__":
+    main()
