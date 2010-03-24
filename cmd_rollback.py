a88c8c61329c5ae8e0a13b59eae6d43fb09f4536
Liraz Siri <liraz@turnkeylinux.org>
2010-03-24 15:29:48 +0200

    rollback: cli stub

---
+++ cmd_rollback.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+"""
+Rollback last restore
+"""
+
+import sys
+import getopt
+
+def fatal(e):
+    print >> sys.stderr, "error: " + str(e)
+    sys.exit(1)
+
+def usage(e=None):
+    if e:
+        print >> sys.stderr, "error: " + str(e)
+
+    print >> sys.stderr, "Syntax: %s" % sys.argv[0]
+    print >> sys.stderr, __doc__.strip()
+    sys.exit(1)
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'h', 
+                                       ['help'])
+                                        
+    except getopt.GetoptError, e:
+        usage(e)
+
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+
+    if args:
+        usage()
+
+
+if __name__=="__main__":
+    main()
