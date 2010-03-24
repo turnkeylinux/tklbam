481b518309667f9f8fe6ab173c6a6206e2935c10
Liraz Siri <liraz@turnkeylinux.org>
2010-03-24 19:17:34 +0200

    implemented filesystem rollback logic

--- cmd_rollback.py
+++ cmd_rollback.py
@@ -33,6 +33,45 @@ def main():
     if args:
         usage()
 
+import os
+import stat
+from changes import Changes
+from restore import Rollback, remove_any
+from dirindex import DirIndex
+
+def test():
+    rollback = Rollback()
+    
+    changes = Changes.fromfile(rollback.fsdelta)
+    dirindex = DirIndex(rollback.dirindex)
+
+    for change in changes:
+        if change.OP == 'o':
+            print "removing overwritten " + change.path
+            remove_any(change.path)
+
+        if change.path not in dirindex:
+            continue
+
+        if change.OP in ('o', 'd'):
+            print "restoring from originals: " + change.path
+            rollback.originals.move_out(change.path)
+
+        dirindex_rec = dirindex[change.path]
+        local_rec = DirIndex.Record.frompath(change.path)
+
+        if dirindex_rec.uid != local_rec.uid or \
+           dirindex_rec.gid != local_rec.gid:
+            print "chown %d:%d %s" % (dirindex_rec.uid, dirindex_rec.gid, change.path)
+            os.lchown(change.path, dirindex_rec.uid, dirindex_rec.gid)
+
+        if dirindex_rec.mod != local_rec.mod:
+            mod = stat.S_IMODE(dirindex_rec.mod)
+            print "chmod %s %s" % (oct(mod), change.path)
+            os.chmod(change.path, mod)
+
+    # delete empty directories
 
 if __name__=="__main__":
-    main()
+    test()
+
