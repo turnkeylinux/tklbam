d4e439431332c12076d3ccc20a6248dae205b164
Liraz Siri <liraz@turnkeylinux.org>
2010-03-28 23:44:28 +0300

    sync with sterile-pylib

--- temp.py
+++ temp.py
@@ -1,24 +1,13 @@
+"""
+Safely create temporary file or directory which is automatically removed
+when the object is dereferenced (by the same process that created it,
+not a subprocess)
+"""
 import os
 from os.path import *
 
-import shutil
 import tempfile
-
-class TempDir(str):
-    def __new__(cls, prefix='tmp', suffix='', dir=None):
-        path = tempfile.mkdtemp(suffix, prefix, dir)
-        self = str.__new__(cls, path)
-        self.pid = os.getpid()
-
-        return self
-
-    def remove(self):
-        if exists(self):
-            shutil.rmtree(self)
-
-    def __del__(self):
-        if self.pid == os.getpid():
-            self.remove()
+import shutil
 
 class TempFile(file):
     def __init__(self, prefix='tmp', suffix=''):
@@ -32,4 +21,21 @@ class TempFile(file):
         # sanity check in case we use fork somewhere
         if self.pid == os.getpid():
             os.remove(self.path)
+
+class TempDir(str):
+    def __new__(cls, prefix='tmp', suffix='', dir=None):
+        path = tempfile.mkdtemp(suffix, prefix, dir)
+        self = str.__new__(cls, path)
+
+        self.pid = os.getpid()
+        self.path = path
+        return self
+
+    def remove(self):
+        if exists(self):
+            shutil.rmtree(self)
+
+    def __del__(self):
+        if self.pid == os.getpid():
+            self.remove()
 
