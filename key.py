92647afd5014159e929c26c9548a22803789c868
Liraz Siri <liraz@turnkeylinux.org>
2010-06-23 10:03:23 +0300

    embed key version, and variable hash/cipher repeat parameters
    rational: future proof the key format
    
    - circumstances might change and we don't want to be stuck with a format
      that hardwires obsolete assumptions. A little foresight now could save
      us a big headache in the future.
    
    - embedding the hash/cipher repeat parameters in the key format allows us
      to parse stronger or weaker keys.
    
    - this allows us in the future to vary the computational expensiveness
      according to needs/usage scenario. Currently the number of repeats is
      still hardwired to cost 0.5 seconds worth of cpu time on my
      workstation.
    
    - embedding a version in the key packet structure costs almost nothing
      and makes it easier to support incompatible changes to the key format
      should the need arise (e.g., changing of cipher/hash, changing the
      format, etc.) while preserving backwards compatibility with old keys.

--- key.py
+++ key.py
@@ -5,10 +5,11 @@ import struct
 
 from Crypto.Cipher import AES
 
-SALT_LEN = 4
+KEY_VERSION = 1
 
-PASSES_HASH = 100000
-PASSES_CIPHER = 100000
+SALT_LEN = 4
+KILO_REPEATS_HASH = 80
+KILO_REPEATS_CIPHER = 80
 
 class Error(Exception):
     pass
@@ -22,15 +23,14 @@ def _unpad(padded):
     len, = struct.unpack("!H", padded[-2:])
     return padded[-(2 + len) :-2]
 
-def _rinserepeat(f, input, count):
+def _repeat(f, input, count):
     for x in xrange(count):
         input = f(input)
     return input
 
-def _cipher_key(passphrase):
-    cipher_key = _rinserepeat(lambda k: hashlib.sha256(k).digest(),  
-                              passphrase, 
-                              PASSES_HASH if passphrase else 1)
+def _cipher_key(passphrase, repeats):
+    cipher_key = _repeat(lambda k: hashlib.sha256(k).digest(),  
+                         passphrase, repeats)
     return cipher_key
 
 def _cipher(cipher_key):
@@ -38,21 +38,41 @@ def _cipher(cipher_key):
 
 def fmt(secret, passphrase):
     salt = os.urandom(SALT_LEN)
-    cipher_key = _cipher_key(passphrase)
+
+    if not passphrase:
+        hash_repeats = cipher_repeats = 1
+    else:
+        hash_repeats = KILO_REPEATS_HASH * 1000 + 1
+        cipher_repeats = KILO_REPEATS_CIPHER * 1000 + 1
+
+    cipher_key = _cipher_key(passphrase, hash_repeats)
     plaintext = salt + hashlib.sha1(secret).digest() + secret
 
-    ciphertext = _rinserepeat(lambda v: _cipher(cipher_key).encrypt(v), 
-                              _pad(plaintext), 
-                              PASSES_CIPHER if passphrase else 1)
+    ciphertext = _repeat(lambda v: _cipher(cipher_key).encrypt(v), 
+                         _pad(plaintext), cipher_repeats)
 
-    return base64.b64encode(ciphertext)
+    packet = struct.pack("!BHH", KEY_VERSION, 
+                         hash_repeats / 1000, 
+                         cipher_repeats / 1000) + ciphertext
+
+    return base64.b64encode(packet)
 
 def parse(formatted, passphrase):
-    ciphertext = base64.b64decode(formatted)
-    cipher_key = _cipher_key(passphrase)
-    decrypted = _rinserepeat(lambda v: _cipher(cipher_key).decrypt(v),
-                             ciphertext,
-                             PASSES_CIPHER if passphrase else 1)
+    packet = base64.b64decode(formatted)
+    version, khr, kcr = struct.unpack("!BHH", packet[:5])
+
+    if version != KEY_VERSION:
+        raise Error("unknown key version (%d)" % version)
+
+    hash_repeats = khr * 1000 + 1
+    cipher_repeats = kcr * 1000 + 1
+    ciphertext = packet[5:]
+
+    cipher_key = _cipher_key(passphrase, hash_repeats)
+    decrypted = _repeat(lambda v: _cipher(cipher_key).decrypt(v),
+                        ciphertext,
+                        cipher_repeats)
+
     decrypted = _unpad(decrypted)
 
     digest = decrypted[SALT_LEN:SALT_LEN+20]
