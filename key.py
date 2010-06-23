e5e740061458b5691a4d38f869a08b146a3138af
Liraz Siri <liraz@turnkeylinux.org>
2010-06-23 08:41:03 +0300

    add computation complexity to encrypt/decrypt itself
    rational: prevent pre-calculation of mapped passphrase keyspace

--- key.py
+++ key.py
@@ -1,32 +1,57 @@
 import os
 import hashlib
 import base64
+import struct
 
 from Crypto.Cipher import AES
 
 SALT_LEN = 4
-HASH_PASSES = 100000
+
+PASSES_HASH = 100000
+PASSES_CIPHER = 200000
 
 class Error(Exception):
     pass
 
+def _pad(s):
+    padded_len = ((len(s) + 2 - 1) |  0xF) + 1
+    padding_len = padded_len - len(s) - 2
+    return os.urandom(padding_len) + s + struct.pack("!H", len(s))
+
+def _unpad(padded):
+    len, = struct.unpack("!H", padded[-2:])
+    return padded[-(2 + len) :-2]
+
+def _rinserepeat(f, input, count):
+    for x in xrange(count):
+        input = f(input)
+    return input
+
 def _cipher_key(passphrase):
-    cipher_key = hashlib.sha256(passphrase).digest()
-    if passphrase:
-        for i in xrange(HASH_PASSES):
-            cipher_key = hashlib.sha256(cipher_key).digest()
+    cipher_key = _rinserepeat(lambda k: hashlib.sha256(k).digest(),  
+                              passphrase, 
+                              PASSES_HASH if passphrase else 1)
     return cipher_key
 
-def _cipher(passphrase):
-    return AES.new(_cipher_key(passphrase), AES.MODE_CFB)
+def _cipher(passphrase, mode=AES.MODE_OFB):
+    return AES.new(_cipher_key(passphrase), mode)
 
 def fmt(secret, passphrase):
     salt = os.urandom(SALT_LEN)
-    return base64.b64encode(_cipher(passphrase).encrypt(salt + hashlib.sha1(secret).digest() + secret))
+    plaintext = salt + hashlib.sha1(secret).digest() + secret
+    ciphertext = _rinserepeat(_cipher(passphrase).encrypt, 
+                              _pad(plaintext), 
+                              PASSES_CIPHER if passphrase else 1)
+
+    return base64.b64encode(ciphertext)
 
 def parse(formatted, passphrase):
     ciphertext = base64.b64decode(formatted)
-    decrypted = _cipher(passphrase).decrypt(ciphertext)
+    decrypted = _rinserepeat(_cipher(passphrase).decrypt,
+                             ciphertext,
+                             PASSES_CIPHER if passphrase else 1)
+    decrypted = _unpad(decrypted)
+
     digest = decrypted[SALT_LEN:SALT_LEN+20]
     secret = decrypted[SALT_LEN+20:]
 
