74df10093f9fb9929db595ee866e55c9d209613c
Liraz Siri <liraz@turnkeylinux.org>
2010-06-24 07:45:38 +0300

    added --random-passphrase to escrow's cli stub
    rational: for extra security you might want to save the keyfile and then save
    the passphrase in another place.

--- cmd_escrow.py
+++ cmd_escrow.py
@@ -3,11 +3,12 @@
 
 Arguments:
 
-    KEYFILE              File path to save the escrow key (- for stdout)
+    KEYFILE                File path to save the escrow key (- for stdout)
 
 Options:
 
-    -P --no-passphrase   Don't encrypt escrow key with a passphrase 
+    -P  --no-passphrase    Don't encrypt escrow key with a passphrase 
+    --random-passphrase    Choose a secure random passphrase (and print it)
 """
 
 import sys
@@ -23,7 +24,7 @@ def usage(e=None):
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hP", ["help", "no-passphrase"])
+        opts, args = getopt.getopt(sys.argv[1:], "hP", ["help", "no-passphrase", "random-passphrase"])
     except getopt.GetoptError, e:
         usage(e)
 
@@ -36,6 +37,8 @@ def main():
     keyfile = args[0]
 
     opt_no_passphrase = False
+    opt_random_passphrase = False
+
     for opt, val in opts:
         if opt in ('-h', '--help'):
             usage()
@@ -43,7 +46,15 @@ def main():
         if opt in ('-P', '--no-passphrase'):
             opt_no_passphrase = True
 
+        if opt == '--random-passphrase':
+            opt_random_passphrase = True
+
+    if opt_no_passphrase and opt_random_passphrase:
+        print >> sys.stderr, "error: --no-passphrase and --random-passphrase are incompatible options"
+        sys.exit(1)
+
     print `opt_no_passphrase`
+    print `opt_random_passphrase`
     print `keyfile`
 
 if __name__ == "__main__":
