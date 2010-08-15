562481f34597062a527f5cdd33eb79e65aba96ba
Liraz Siri <liraz@turnkeylinux.org>
2010-08-15 18:51:33 +0300

    Revert "internal command cli and regtest"
    This reverts commit cde70250c9bddb9c9ca3d053e66ecea07335eb88.

--- cmd_internal.py
+++ cmd_internal.py
@@ -4,28 +4,24 @@
 # 
 # This file is part of TKLBAM (TurnKey Linux BAckup and Migration).
 # 
-# TKLBAM is open source software; you can redistribute it and/or
+# TKLBAM is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 3 of
 # the License, or (at your option) any later version.
 # 
 """
-TurnKey Linux Backup and Migration
+Execute an internal command
 """
-from os.path import *
+import os
 from cliwrapper import CliWrapper
+
+import cmd_internals
 
 class CliWrapper(CliWrapper):
     DESCRIPTION = __doc__
-    PATH = [ dirname(realpath(__file__)) ]
-    
-    COMMANDS_USAGE_ORDER = ['init',
-                            '',
-                            'passphrase', 'escrow',
-                            '',
-                            'backup', 'list', 'restore', 'restore_rollback',
-                            '',
-                            'internal']
+    PATH = cmd_internals.__path__
 
 if __name__ == "__main__":
     CliWrapper.main()
+
+
