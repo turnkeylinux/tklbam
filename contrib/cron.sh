34ed78c5a50a3c3a0ba1083bee375244fcad107c
Liraz Siri <liraz@turnkeylinux.org>
2012-10-10 04:37:00 +0200

    Added jitter to tklbam-backup cron job + safely overwrite non-jittered versions

--- contrib/cron.sh
+++ contrib/cron.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
+set ${JITTER:=3600}
+
+randomsleep() {
+    MAXSLEEP=$1
+
+    if [ -n "$MAXSLEEP" ] ; then
+        if [ $MAXSLEEP -gt 0 ] ; then
+            if [ -z "$RANDOM" ] ; then
+                # A fix for shells that do not have this bash feature.
+                RANDOM=$(dd if=/dev/urandom count=1 2> /dev/null | cksum | cut -c"1-5")
+            fi
+            TIME=$(($RANDOM % $MAXSLEEP))
+            sleep $TIME
+        fi
+    fi
+}
+
+# prevent everyone from hitting the Hub API at exactly the same time...
+randomsleep $JITTER
 tklbam-backup --quiet
