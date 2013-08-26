33c634c2f07b7c053f76f2233097c2dfb1f25f91
Liraz Siri <liraz@turnkeylinux.org>
2013-08-26 05:41:00 +0300

    updated copyright date

--- cmd_restore.py
+++ cmd_restore.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2010-2012 Liraz Siri <liraz@turnkeylinux.org>
+# Copyright (c) 2010-2013 Liraz Siri <liraz@turnkeylinux.org>
 #
 # This file is part of TKLBAM (TurnKey Linux BAckup and Migration).
 #
