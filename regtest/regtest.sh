c6a8c81c0a771790fd1f47496a3c878c78ef807a
Jeremy Davis <jeremy@turnkeylinux.org>
2018-12-03 07:59:08 +1100

    add failure if test depends missing

--- regtest/regtest.sh
+++ regtest/regtest.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+fatal() { echo "FATAL: $@"; exit 1; }
+
+DEPS="faketime fakeroot"
+for dep in $DEPS; do
+    [ "$(which $dep)" ] || fatal "missing dependency: $dep"
+done
+
 # faketime messes with Python's object caching mechanism
 cd ../
     make clean > /dev/null
 cd - > /dev/null
-faketime -f "2000-1-1 00:00:00" fakeroot $(dirname $0)/_regtest.sh $@
+faketime -f '2000-1-1 00:00:00' fakeroot $(dirname $0)/_regtest.sh $@
