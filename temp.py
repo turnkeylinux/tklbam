530517cb83d6d19f377c2ae453dc055e2728dc54
Jeremy Davis <jeremy@turnkeylinux.org>
2023-08-28 10:31:02 +0000

    add temp

---
+++ temp.py
@@ -0,0 +1,43 @@
+"""
+Safely create temporary file or directory which is automatically removed
+when the object is dereferenced (by the same process that created it,
+not a subprocess)
+"""
+import os
+from os.path import *
+
+from io import FileIO
+import tempfile
+import shutil
+
+
+class TempFile(FileIO):
+    def __init__(self, prefix='tmp', suffix=''):
+        fd, path = tempfile.mkstemp(suffix, prefix)
+        os.close(fd)
+        self.path = path
+        self.pid = os.getpid()
+        super().__init__(self, path, "w")
+
+    def __del__(self):
+        # sanity check in case we use fork somewhere
+        if self.pid == os.getpid():
+            os.remove(self.path)
+
+
+class TempDir(str):
+    def __new__(cls, prefix='tmp', suffix='', dir=None):
+        path = tempfile.mkdtemp(suffix, prefix, dir)
+        self = str.__new__(cls, path)
+
+        self.pid = os.getpid()
+        self.path = path
+        return self
+
+    def remove(self):
+        if exists(self):
+            shutil.rmtree(self)
+
+    def __del__(self):
+        if self.pid == os.getpid():
+            self.remove()
